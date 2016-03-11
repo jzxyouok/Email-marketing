bc7599f8cc09198d4bdf04fc09bcff84fbd67178
248278242@qq.com <248278242@qq.com>
2016-03-11 12:09:27 +0800

    更新发件人组

--- app/senders/controller/Sendergroup.php
+++ app/senders/controller/Sendergroup.php
@@ -3,5 +3,18 @@ namespace app\senders\controller;
 
 class Sendergroup
 {
+	public function index(){
+			$mailsendergroup = M('mailsendergroup');
+			$count = $mailsendergroup->count();
+			$view = new \think\View();
+			$view->assign('count',$count);
+			$Page  = new \think\Pagehome($count,10);
+			$show  = $Page->show();
+			$mailsendergroupdata  = $mailsendergroup->limit($Page->firstRow.','.$Page->listRows)->select();
 
+			//var_dump($Accountdata);
+			$view->assign('page',$show);
+			$view->assign('data',$mailsendergroupdata);
+			return $view->fetch();
+	}
 }
